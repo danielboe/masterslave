198aadc7751eab2e8e978730d36f49de2278c959
danielboe <danielboe@gmx.de>
2015-09-09 17:38:48 +0200

    sync

--- masterslave.ino
+++ masterslave.ino
@@ -1,32 +1,8 @@
-
-
-void setup()
-{
- Serial.begin(9600);
- Serial.println("init");
-}
-
-void loop()
-{ 
-  
-
-    while (Serial.available()) {
-      int ch = Serial.read()-48;
-      if (ch == 1) {
-        float ampere = get_ampere(3);
-        s_print_ampere(ampere);
-      }
-      //memset(intBuffer, 0, sizeof(intBuffer)); //clear array
-      if (ch == 2) {
-        set_relais(1,1);
-      }
-      if (ch == 3) {
-        set_relais(1,0);
-      }
-    }
-}
-
-
+//test1
+#include <EEPROM.h>
+#include <Wire.h>    // I2C-Bibliothek einbinden
+#include "RTClib.h"  // RTC-Bibliothek einbinden
+RTC_DS1307 RTC;      // RTC Modul
 
 float get_ampere(int x){
   //par_1=messure pin, output value in amp float
@@ -51,7 +27,48 @@ float get_ampere(int x){
   rms = sqrt((float)currentAcc/(float)numSamples) * (75.7576 * 0.3297 / 1024.0 );
   return rms;
 }
-
+void set_relais(int x, int y){
+  //offset pins +4 (start pin 5)
+  //par_1 = relais no, par_2 = set 1/0 or switch;
+  int currentPin = constrain(x+4, 5, 12);
+  int invert = 1;
+  int test = 0;
+  pinMode(currentPin, OUTPUT);
+  digitalWrite(currentPin, LOW);
+  if (y == 1) {
+    digitalWrite(currentPin, LOW);
+    EEPROM.write(currentPin-4,0);
+  }
+  if (y == 0) {
+    digitalWrite(currentPin, HIGH);
+    EEPROM.write(currentPin-4,1);
+  }
+   if (y > 1) {
+    invert = EEPROM.read(currentPin-4);
+    invert = !invert;
+    digitalWrite(currentPin, invert);
+    EEPROM.write(currentPin-4,invert);
+  }
+}
+void load_relais() {
+  byte pin = 5;
+  byte state = 0;
+  while (pin <= 12) {
+    state = EEPROM.read(pin-4);
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, state);
+    pin = pin+1;
+  }
+}
+void save_relais() {
+  byte pin = 5;
+  byte state = 0;
+  while (pin <= 12) {
+    state = digitalRead(pin);
+    EEPROM.write(pin-4, state);
+    pin = pin+1;
+  }
+}
 void s_print_ampere(float x) {
   float store = x;
   if (store > 0.18) {
@@ -67,18 +84,113 @@ void s_print_ampere(float x) {
     Serial.println("off");
   }
 }
-
-void set_relais(int x, int y){
-  //offset pins +4 (start pin 5)
-  //par_1 = relais no, par_2 = set 1/0;
-  int currentPin = x+4;
-  pinMode(currentPin, OUTPUT);
-  digitalWrite(currentPin, LOW);
-  if (y == 1) {
-    digitalWrite(currentPin, LOW);
-  }
-  else {
-    digitalWrite(currentPin, HIGH);
+void s_print_time() {
+        DateTime now=RTC.now(); // aktuelle Zeit abrufen
+        Serial.print(now.hour(), DEC);
+        Serial.print(':');
+        Serial.print(now.minute(), DEC);
+        Serial.print(':');
+        Serial.print(now.second(), DEC);
+        Serial.println();
+}
+void check_eeprom(int x) {
+  int address = 0;
+  byte value;
+  while (address <= x) {
+    value = EEPROM.read(address);  
+    Serial.print(address);
+    Serial.print(" ");
+    Serial.print(value, DEC);
+    Serial.println();
+    // advance to the next address of the EEPROM
+    address = address + 1; 
+    // there are only 512 bytes of EEPROM, from 0 to 511, so if we're
+    // on address 512, wrap around to address 0
   }
 }
+
+void setup()
+{
+ Serial.begin(9600);
+ Serial.println("init 1.4");
+ Wire.begin();
+ RTC.begin();
+ if (! RTC.isrunning()) {
+   RTC.adjust(DateTime(__DATE__, __TIME__));
+   Serial.println("Echtzeituhr gestartet Systemzeit gesetzt.");
+ }
+ load_relais();
+}
+
+int test = 1;
+
+void loop()
+{ 
+  
+  while (Serial.available() > 0) {
+
+    int input1 = Serial.parseInt();
+    int input2 = Serial.parseInt();
+    int input3 = Serial.parseInt();
+    if (Serial.read() == '\n') {
+
+      if (input1 == 1) {
+        float ampere = get_ampere(3);
+        s_print_ampere(ampere);
+      }
+      if (input1 == 2) {
+        set_relais(input2,input3);
+      }
+      if (input1 == 3) {
+        save_relais();
+      }
+      if (input1 == 4) {
+
+      }
+      if (input1 == 5) {
+        while (Serial.available() <= 0) {
+          Serial.print("              ");
+          s_print_time();
+          int loop = 0;
+          while (loop <= 8) {
+            Serial.println();
+            loop = loop + 1;
+          }
+          delay(1000);
+        }
+
+      }
+      //debug
+      if (input1 == 0) {
+        Serial.println("---------------");
+        s_print_time();
+        Serial.print("input:");
+        Serial.print(" ");
+        Serial.print(input1);
+        Serial.print(" ");
+        Serial.print(input2);
+        Serial.print(" ");
+        Serial.println(input3);
+        while (test <=8) {
+          Serial.print("Status_pin_");
+          Serial.print(test);
+          Serial.print(": ");
+          Serial.println(digitalRead(test+4)); 
+          test = test+1;
+        }
+        test = 1; 
+        check_eeprom(input2);
+        Serial.println("---------------");
+      }
+    }
+  }
+  
+}
+
+
+
+
+
+
+
 
